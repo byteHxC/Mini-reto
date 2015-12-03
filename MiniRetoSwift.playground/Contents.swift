45d0442fc9df96e1af1761788e99fbd4cf19cc50
Byter <Byter@MacBook-Pro-de-Byter.local>
2015-12-03 16:39:37 -0600

    Plagraunds in github

---
+++ MiniRetoSwift.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var range = 0...100
+print("")
+print("Con for")
+for value in range {
+    
+    if value>=30 && value<=40{
+        print(" #\(value) Viva swift!!")
+    }else if value%5 == 0 {
+        print(" #\(value) Bingo !!")
+    }else if value%2 == 0{
+        print(" #\(value) Par!!")
+    }else if value%2 != 0{
+        print(" #\(value) Impar!!")
+    }
+    
+}
